ea57c9614c94ffced7a2a543d98961427a99aff0
Anton Bahayeuski <antnbog@gmail.com>
2023-04-01 05:32:33 +0400

    Fix script

--- utils/autologout.py
+++ utils/autologout.py
@@ -14,14 +14,8 @@ def main():
         last_activity_time = datetime.strptime(user['lastLogin']['$date'], '%Y-%m-%dT%H:%M:%S.%fZ')
         last_status = user['status']
 
-        if last_activity_time < time_threshold and user['status'] != 'online':
-            token = rocket.login(user['username'], 'password').json()['data']['authToken']
-            
+        if last_activity_time < time_threshold and last_status != 'online':
             rocket.logout_user(user['username'])
-            requests.delete('https://rocketchat.sitetesting.fun/_session/' + token, headers={'Cookie': 'rc_uid=' + user['username'] + '; rc_token=' + token + ';'})
 
 while True:
-    try:
-        main()
-    except Exception as e:
-        print(f'error: {e}')
+    main()
