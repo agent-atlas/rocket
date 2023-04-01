d8c06ed4731f5cd2081d504b1e83cfaec3fd5ebb
Anton Bahayeuski <antnbog@gmail.com>
2023-04-01 06:07:49 +0400

    Overide script

--- utils/autologout.py
+++ utils/autologout.py
@@ -2,23 +2,49 @@ from rocketchat_API.rocketchat import RocketChat
 import requests
 from datetime import datetime, timedelta
 
+import time
+from rocketchat_API.APIExceptions.RocketExceptions import RocketConnectionException
+from rocketchat_API.APIExceptions.RocketAuthenticationException import RocketAuthenticationException
+from rocketchat_API.APIExceptions.RocketMissingParamException import RocketMissingParamException
+from rocketchat_API import RocketChatAPI
+
 def main():
-    rocket = RocketChat('ivat', 'Pbvf94', server_url='https://rocketchat.sitetesting.fun', ssl_verify=False)
+    api = RocketChatAPI(settings={
+    "username": "ivat",
+    "password": "Pbvf94",
+    "domain": "rocketchat.sitetesting.fun",
+})
+
+    # rocket = RocketChat('ivat', 'Pbvf94', server_url='https://rocketchat.sitetesting.fun', ssl_verify=False)
 
     timeout_minutes = 2
     time_threshold = datetime.now() - timedelta(minutes=timeout_minutes)
-    
-    online_users = rocket.users_list(filters={'status': 'online'}).json()['users']
-
-    for user in online_users:
-        last_activity_time = datetime.strptime(user['lastLogin']['$date'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        last_status = user['status']
-
-        if last_activity_time < time_threshold and last_status != 'online':
-            rocket.logout(user['username'])
-
+    # users_online = api.users_list(status='online').json()
+    online_users = api.users_list(status='online').json()
+    for user in online_users["users"]:
+        # получение информации о пользователе
+        try:
+            user_info = api.users_info(user["username"]).json()
+        except (RocketConnectionException, RocketAuthenticationException, RocketMissingParamException) as e:
+            print(f"Error: {e}")
+            continue
+        
+        # получение времени последней активности пользователя
+        last_activity = user_info["user"]["lastLogin"]["$date"] / 1000.0
+        current_time = time.time()
+        
+        # проверка, прошло ли более 2 минут с последней активности пользователя
+        if current_time - last_activity > 120:
+            # отправка запроса на логаут пользователя
+            try:
+                api.logout_user(user_id=user_info["user"]["_id"])
+                print(f"User {user_info['user']['username']} was logged out")
+            except (RocketConnectionException, RocketAuthenticationException, RocketMissingParamException) as e:
+                print(f"Error: {e}")
+                continue
 while True:
     try:
         main()
+        time.sleep(120)
     except Exception as e:
         print('Error:, {}'.format(e))
