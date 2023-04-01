ad9f0d2c72c3ad856d06affad2ba82d7d27ef1c7
Anton Bahayeuski <antnbog@gmail.com>
2023-04-01 14:14:41 +0400

    try fix relative imports

--- src/autologout.py
+++ src/autologout.py
@@ -6,7 +6,7 @@ import time
 from rocketchat_API.APIExceptions.RocketExceptions import RocketConnectionException, RocketAuthenticationException, \
     RocketMissingParamException
 
-from app.adapter import RocketChatApi
+from adapter import RocketChatApi
 
 
 def main():
