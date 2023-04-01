813d8eba6a53e578527b75131255e27a8a071a5d
Anton Bahayeuski <antnbog@gmail.com>
2023-04-01 14:21:27 +0400

    fix cmd Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,6 @@ COPY requirements.txt .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
+COPY src/autologout.py .
 
-CMD ["python", "src.autologout"]
+CMD ["python", "./autologout.py"]
--- src/autologout.py
+++ src/autologout.py
@@ -1,3 +1,4 @@
+import hashlib
 import traceback
 from datetime import datetime, timedelta
 
@@ -6,7 +7,19 @@ import time
 from rocketchat_API.APIExceptions.RocketExceptions import RocketConnectionException, RocketAuthenticationException, \
     RocketMissingParamException
 
-from .adapter import RocketChatApi
+from rocketchat_API.rocketchat import RocketChat
+
+
+class RocketChatApi(RocketChat):
+    def __init__(self, user=None, password=None, auth_token=None, user_id=None, server_url='http://127.0.0.1:3000',
+                 ssl_verify=True, proxies=None, timeout=30, session=None, client_certs=None):
+        """Creates a RocketChat object and does login on the specified server"""
+        super().__init__(user, password, auth_token, user_id, server_url, ssl_verify, proxies, timeout, session,
+                         client_certs)
+
+        if password:
+            self.headers['X-2fa-code'] = hashlib.sha256(password.encode('utf-8')).hexdigest()
+            self.headers['X-2fa-method'] = 'password'
 
 
 def main():
